03fd608c7a39648536c8542029532b437817f0ae
edumats <eduardo.mats@gmail.com>
2019-09-05 11:11:08 -0300

    Working login and register

--- application.py
+++ application.py
@@ -1,108 +1,71 @@
 import os
 
-import psycopg2
+from flask import Flask, flash, session, request, redirect, render_template, session, url_for, jsonify
+from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+import requests
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required
-import datetime
 
-# Configure application
+res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "tazZgVasmaEMamfyYoS7Rg", "isbns": "9781632168146"})
+print(res.json())
+
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
 
-# Ensure responses aren't cached
-
-
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_FILE_DIR"] = mkdtemp()
+# Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# database engine object from SQLAlchemy that manages connections to the database
-# DATABASE_URL is an environment variable that indicates where the database lives
-engine = create_engine("postgres://ciyhgcghdmlvvk:ff972a3b363002679b9a33dca4a0fcc5caf8b4095f03727de998c5f1dd58989a@ec2-54-221-201-212.compute-1.amazonaws.com:5432/ddsqmt6fvh3id5")
-
-# create a 'scoped session' that ensures different users' interactions with the database are kept separate
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"),echo=True)
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 @login_required
 def index():
-    """Show graph"""
-
-    """ Requests sensor data from database """
-    data = db.execute('SELECT * FROM sensor_data').fetchall()
-    if not data:
-        return apology("Error retrieving sensor data", 400)
-
-    return render_template("index.html", data=data)
-
-@app.route("/check", methods=["GET"])
-def check():
-    """Return true if username available, else false, in JSON format"""
-    if request.method == 'GET':
-        username = request.args.get('username')
-        # Query database for username
-        query = db.execute('SELECT username FROM users WHERE username = :username', {"username": username})
-
-        # If username is lenght > 1 and does not contain in users database, return true; otherwise, false
-        if len(username) > 1 and not query:
-            return jsonify(True)
-        else:
-            return jsonify(False)
+    """ Main Page """
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
+    """ Logs user """
 
-    # Forget any user_id
+    # Forgets user id, if any
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
+    # Reached via POST method
     if request.method == "POST":
+
         username = request.form.get("username")
-        # Ensure username was submitted
+        password = request.form.get("password")
+
+        # Check if username is provided
         if not username:
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
+            return apology("please type a username", 400)
+        
+        # Check if password is provided
+        if not password:
             return apology("must provide password", 403)
-
+        
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          {"username":username})
-
-        # Ensure username exists and password is correct
-        if not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
+                          {"username":username}).fetchone()
+        
+        if rows is not None and check_password_hash(rows.hash, password):
+            session["user_id"] = rows.id
+            return redirect("/")
+        
+        flash('Wrong username or password')
+        return redirect(url_for('login'))
+        
     else:
         return render_template("login.html")
-
 
 @app.route("/logout")
 def logout():
@@ -114,106 +77,63 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
+    """ Register user """
 
-    # Forget any user_id
+    # Forget cached session
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
+    # Reached via POST method
     if request.method == "POST":
+        
+        username = request.form.get("username")
 
-        username = request.form.get('username')
-        # Ensure username was submitted
+        # Check if username is provided
         if not username:
-            return apology("must provide username", 400)
+            return apology("please type a username", 400)
 
-        # Ensure password was submitted
+        # Check if password is provided
         if not request.form.get("password"):
-            return apology("must provide password", 400)
-
+            return apology("please type a password", 400)
+        
+        # Check if password matches in confirmation field
         if request.form.get("password") != request.form.get("confirmation"):
             return apology("passwords do not match", 400)
+        
+        # Check if user exists in database
+        user = db.execute('SELECT username FROM users WHERE username=:username', {"username":username}).fetchone()
+        if user is not None:
+            return apology("user already exists", 400)
 
-        user = db.execute('SELECT username FROM users WHERE username=:username', {"username":username})
-        if user:
-            return apology('user already exists', 400)
-
+        # Create hash from password
         hash = generate_password_hash(request.form.get("password"))
 
-        # Query database for username
-        result = db.execute("INSERT INTO users(username, hash) VALUES(:username, :hash)",
-                            {"username":username, "hash":hash})
-        if not result:
-            return apology("user already exists", 200)
+        # Store new user into database
+        result = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash) RETURNING id",
+                            {"username":username, "hash":hash}).fetchone()
         db.commit()
 
-        # Stores id returned by INSERT
-        session["user_id"] = result
+        # Stores id in session
+        session["user_id"] = result[0]
 
         # Redirect user to home page
-        flash("Registration successful")
-        return redirect(url_for('index'))
+        return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
+    # Reached route via GET
     else:
         return render_template("register.html")
 
-@app.route("/settings", methods=["GET", "POST"])
-@login_required
-def settings():
-    '''Manages users settings'''
-    if request.method == 'POST':
+@app.route("/check", methods=["GET"])
+def check():
+    """Return true if username available, else false, in JSON format"""
+    if request.method == 'GET':
+        username = request.args.get('username')
+        # Query database for username
+        query = db.execute('SELECT username FROM users WHERE username = :username', {"username":username}).fetchone()
 
-        id = session["user_id"]
-        password = request.form.get("password")
-        if not password:
-            flash('You must provide the current password')
-            return redirect("/settings")
-
-        password_new = request.form.get("password_new")
-        if not password_new:
-            flash('You must provide the new password')
-            return redirect("/settings")
-
-        # Query database
-        rows = db.execute("SELECT * FROM users WHERE id = :id",
-                          {"id":id})
-        print(rows)
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid password", 403)
-
-        # Hash new password
-        hash = generate_password_hash(request.form.get("password_new"))
-
-        # Update password
-        update = db.execute('UPDATE users SET hash = :hash WHERE id = :id', {"hash":hash, "id":id})
-        db.commit()
-        if update:
-            # Redirect user to home page
-            flash("Password have successfully changed")
-            return redirect(url_for('index.html'))
+        # If username is lenght > 1 and does not contain in users database, return true; otherwise, false
+        if len(username) > 1 and not query:
+            return jsonify(True)
         else:
-            flash("Error updating password")
-            return redirect(url_for('index.html'))
-    else:
-        return render_template('settings.html')
-
-
-def errorhandler(e):
-    """Handle error"""
-    if not isinstance(e, HTTPException):
-        e = InternalServerError()
-    return apology(e.name, e.code)
-
-
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+            return jsonify(False)
--- helpers.py
+++ helpers.py
@@ -1,6 +1,3 @@
-import requests
-import urllib.parse
-
 from flask import redirect, render_template, request, session
 from functools import wraps
 
@@ -10,7 +7,6 @@ def apology(message, code=400):
     def escape(s):
         """
         Escape special characters.
-
         https://github.com/jacebrowning/memegen#special-characters
         """
         for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
@@ -23,7 +19,6 @@ def apology(message, code=400):
 def login_required(f):
     """
     Decorate routes to require login.
-
     http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
     """
     @wraps(f)
